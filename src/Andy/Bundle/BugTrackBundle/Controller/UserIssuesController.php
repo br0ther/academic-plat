a8dbfd616e9e088d7018a67f3ecd5f2474c3f622
br0ther <brotherrrr@gmail.com>
2016-06-14 18:19:07 +0300

    Add collaborators functionality, processes, handlers, grids

---
+++ src/Andy/Bundle/BugTrackBundle/Controller/UserIssuesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Andy\Bundle\BugTrackBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+
+/**
+ * @Route("/issue")
+ */
+class UserIssuesController extends Controller
+{
+    /**
+     * @Route("/user/{userId}", name="user_issues", requirements={"userId"="\d+"})
+     * @AclAncestor("issue_view")
+     * @Template
+     * @param int $userId
+     * @return array
+     */
+    public function issuesAction($userId)
+    {
+        return [
+            'userId' => $userId,
+            'entity' => $this->getUser()
+        ];
+    }
+}
--- src/Andy/Bundle/BugTrackBundle/Handler/IssueHandler.php
+++ src/Andy/Bundle/BugTrackBundle/Handler/IssueHandler.php
@@ -5,11 +5,15 @@ namespace Andy\Bundle\BugTrackBundle\Handler;
 use Andy\Bundle\BugTrackBundle\Entity\Issue;
 use Doctrine\ORM\EntityManager;
 use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
+use Oro\Bundle\NoteBundle\Entity\Note;
+use Oro\Bundle\UserBundle\Entity\UserInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 /**
  * Class IssueHandler
  */
-class IssueHandler extends BaseHandler
+class IssueHandler
 {
     /**
      * @var EntityManager
@@ -17,14 +21,35 @@ class IssueHandler extends BaseHandler
     protected $em;
 
     /**
+     * @var TokenInterface
+     */
+    private $token;
+
+    /**
      * Constructor.
      *
      * @param EntityManager $em
+     * @param TokenStorage  $tokenStorage
      */
-    public function __construct(EntityManager $em)
+    public function __construct(EntityManager $em, TokenStorage $tokenStorage)
     {
         $this->em = $em;
+        $this->token = $tokenStorage->getToken();
     }
+
+    /**
+     * Get current user
+     *
+     * @return UserInterface|null
+     */
+    protected function getUser()
+    {
+        if ($this->token) {
+            return $this->token->getUser();
+        }
+
+        return null;
+    }    
 
     /**
      * @param bool $isSubtask
@@ -50,4 +75,41 @@ class IssueHandler extends BaseHandler
         return $qb->getQuery()->getResult();
     }
 
+    /**
+     * @param Issue $issue
+     */
+    public function addIssueCollaborators(Issue $issue)
+    {
+        $collaborators = [];
+        $collaborators[] = $issue->getReporter();
+        $collaborators[] = $issue->getAssignee();
+
+        foreach ($collaborators as $collaborator) {
+            $issue->addCollaborator($collaborator);
+        }
+
+        $this->em->flush();
+    }
+
+    /**
+     * @param Note $note
+     */
+    public function addNoteCollaborators(Note $note)
+    {
+        $issue = $note->getTarget();
+
+        $issue->addCollaborator($this->getUser());
+
+        $this->em->flush();
+    }
+
+    public function setIssueUpdatedAtWithNote(Note $note)
+    {
+        /** @var Issue $issue */
+        $issue = $note->getTarget();
+
+        $issue->setUpdatedAt(new \DateTime('now'));
+
+        $this->em->flush();
+    }
 }
